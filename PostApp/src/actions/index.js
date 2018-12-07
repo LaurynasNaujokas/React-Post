16b630da70b50610852d066513a137cd9cfcb458
Laurynas <31208060+Skywalker190@users.noreply.github.com>
2018-12-07 19:09:18 +0100

    Update index.js

--- PostApp/src/actions/index.js
+++ PostApp/src/actions/index.js
@@ -3,10 +3,8 @@ export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
 export const FETCH_POST = 'fetch_post';
 export const DELETE_POST = 'delete_post'; 
-
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=magadan90';
-
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
     return {
